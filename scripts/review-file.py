1abbd67f72306965bf78496a3f70450522ce1fc5
lauragut22 <laura.gut@web.de>
2025-09-09 16:08:53 +0200

    Update review-file.py
    rearranged the conditions

--- scripts/review-file.py
+++ scripts/review-file.py
@@ -1,10 +1,10 @@
 age = int(input("How old are you? "))
 
-if age >= 20:
-    print("how is your quarter life crisis going?")
-elif age >= 26:
+if age >= 26:
     print("wow! congratulations, you are now officially a potential unc!!:-))")
 elif age == 23:
     print("juhu, we are the same age!")
+elif age >= 20:
+    print("how is your quarter life crisis going?")
 else:
     print("keck hehe")

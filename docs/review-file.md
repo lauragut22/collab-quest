52de085902baa6d55886828becdd88b667cd1758
Laura Gut <lauragut@host-134-107-73-128.windows.mppmu.mpg.de>
2025-09-09 15:43:50 +0200

    this is a great file

---
+++ docs/review-file.md
@@ -0,0 +1,8 @@
+The file asks your age and gives some sweet little comments about it. 
+The different scenarios are:
+1. you are under 21
+2. you are exactly 23
+3. you are 21-25
+4. you are 26 or older
+
+For each scenario there should be a different answer.
---
+++ scripts/review-file.py
@@ -0,0 +1,10 @@
+age = int(input("How old are you? "))
+
+if age >= 20:
+    print("how is your quarter life crisis going?")
+elif age >= 26:
+    print("wow! congratulations, you are now officially a potential unc!!:-))")
+elif age == 23:
+    print("juhu, we are the same age!")
+else:
+    print("keck hehe")
